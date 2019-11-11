f9e38b8262d535c041e2e08d0ddb77ace43270e4
Amaury <dakub1984@gmail.com>
2019-11-11 20:51:38 +0100

    PaginationService added

--- src/Service/PaginationService.php
+++ src/Service/PaginationService.php
@@ -2,7 +2,9 @@
 
 namespace App\Service;
 
+use Twig\Environment;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class PaginationService 
 {
@@ -10,14 +12,33 @@ class PaginationService
   private $limit = 10;
   private $currentPage = 1;
   private $manager;
+  private $twig;
+  private $route;
+  private $templatePath;
 
-  public function __construct(ObjectManager $manager)
+  public function __construct(ObjectManager $manager, Environment $twig, RequestStack $request, $templatePath)
   {
     $this->manager = $manager;
+    $this->twig = $twig;
+    $this->route = $request->getCurrentRequest()->attributes->get('_route');
+    $this->templatePath = $templatePath;
+  }
+
+
+  public function display()
+  {
+    $this->twig->display($this->templatePath, [
+      'page' => $this->currentPage,
+      'pages' => $this->getPages(),
+      'route' => $this->route
+    ]);
   }
 
   public function getPages()
-  {
+  { 
+    if(empty($this->entityClass)) {
+      throw new \Exception("Vous n'avez pas spécifié l'entitée sur laquelle vous voulez paginer, utilisez la methode setEntityClass du PaginationService !");
+    }
     // Total pages of an entity for pagination
     $repository = $this->manager->getRepository($this->entityClass);
     $total = count($repository->findAll());
@@ -29,6 +50,9 @@ class PaginationService
 
   public function getData()
   { 
+    if(empty($this->entityClass)) {
+      throw new \Exception("Vous n'avez pas spécifié l'entitée sur laquelle vous voulez paginer, utilisez la methode setEntityClass du PaginationService !");
+    }
     // offset
     $offset = $this->currentPage * $this->limit - $this->limit;
     // get elements with the repository
@@ -36,6 +60,28 @@ class PaginationService
     $data = $repository->findBy([], [], $this->limit, $offset);
 
     return $data;
+  }
+
+  public function setTemplatePath($templatePath)
+  {
+    $this->templatePath = $templatePath;
+    return $this;
+  }
+
+  public function getTemplatePath()
+  {
+    return $this->templatePath;
+  }
+
+  public function setRoute($route)
+  {
+    $this->route = $route;
+    return $this;
+  }
+
+  public function getRoute()
+  {
+    return $this->route;
   }
 
   public function setCurrentPage($currentPage)
